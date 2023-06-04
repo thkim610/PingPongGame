3c6b602f2126de16f0cef4db345659f63888f64c
thkim610 <thkim610@naver.com>
2023-06-04 13:35:48 +0900

    KeyListener 및 Thread 처리

--- src/pongV2/Tennis.java
+++ src/pongV2/Tennis.java
@@ -3,11 +3,20 @@ package pongV2;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Tennis extends Applet {
+public class Tennis extends Applet implements Runnable, KeyListener {
 	final int WIDTH = 700, HEIGHT = 500;
+	Thread thread;
+	
 	public void init() {
 		this.resize(WIDTH, HEIGHT); // 게임화면 크기 초기화
+		
+		this.addKeyListener(this);
+		
+		thread = new Thread(this);
+		thread.start();
 	}
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
@@ -16,5 +25,37 @@ public class Tennis extends Applet {
 	
 	public void update(Graphics g) {
 		paint(g);
+	}
+	@Override
+	public void run() {
+		for(;;) {
+			
+			repaint();
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			
+		}
+	}
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			
+		}
+		
 	}
 }
